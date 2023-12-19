8f0af9386ddf7a9f009b9da0e3f973efee203959
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-12-19 09:52:42 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,3 +3,11 @@ set -o allexport; source .env; set +o allexport;
 
 echo "Creating user..."
 sleep 240s;
+
+# set env vars
+set -o allexport; source .env; set +o allexport;
+
+echo "Creating user..."
+sleep 240s;
+
+
